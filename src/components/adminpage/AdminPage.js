52d32664fd3dbcc714e93b9676e88df459256b1b
Mesut Yeşilören <125920240+Tomrisak@users.noreply.github.com>
2024-04-18 19:34:58 +0300

    AdminPage

--- src/components/adminpage/AdminPage.js
+++ src/components/adminpage/AdminPage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import video from './mesut.mp4';
+import voice from './mesut1.mp3';
 
 const AdminPage = () => {
-
-
   return (
-    <div style={{ textAlign: "center", marginTop: "40vh", transform: "translateY(-50%)" }}>
-      <h1>Admin Sayfasına Hoşgeldin </h1>
+    <div style={{ width: "100%", height: "89vh", display: "flex", justifyContent: "center", alignItems: "center" }}> 
+      <video src={video} autoPlay loop muted style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+      <audio src={voice} autoPlay loop controls style={{ width: "0", height: "0", visibility: "hidden" }} />
     </div>
   );
 };
